90d3e78cedaa0606e8f9b99d78ee2d5e2fd5e4b9
AlehZahorski <git3371m@gmail.com>
2020-04-15 14:39:03 +0200

    full lesson is ready

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 const inputPassword = document.getElementById('passInput');
 const textOut = document.getElementById('textOut');
 
-const password = ["qwe","asd"]
-const message = ["Dobrze ze jestes, program juz jest gotowy czekalismy tylko na ciebie!", "Czesc, milo cie widziec."]
+const password = ["Winter", "sprINg", "sUMmer", "auTUmn"]
+const message = ["Oh hell, it's so cold!", "Great time of year, warm, everything blooms!", "The best time of the year, sunny, warm!", "Well .. winter is coming"]
 
-inputPassword.addEventListener('input', (e) => {
-   if(password[0] === e.target.value){
-       textOut.textContent=(message[0])
-   }else if(password[1] === e.target.value){
-    textOut.textContent=(message[1])
-   }else{
-    textOut.textContent= ''
-   }
-})
+
+////////////// rozwiazanie od Samuraja ////////////////
+// const mapIsReallyCool= password.map(i => i.toLowerCase())
+// const showMessage = (e) => {
+//     const inputPassword = e.target.value.toLowerCase();
+//     for( let i = 0; i < password.length; i++){
+//         if(inputPassword === mapIsReallyCool[i]){
+//             textOut.textContent = message[i];
+//         }
+//     }
+// }
+// inputPassword.addEventListener('input', showMessage)
+
+
+
+/////////////// moje rozwiazanie ///////////////////////
+inputPassword.addEventListener('input', () => {
+   textOut.textContent= '';
+   password.forEach((passFirstIndex, indexFirstIndex) => {
+       if(passFirstIndex.toLowerCase() === event.target.value.toLowerCase()){
+           textOut.textContent = message[indexFirstIndex];
+       }else if(passFirstIndex.toUpperCase() === event.target.value.toUpperCase()){
+        textOut.textContent = message[indexFirstIndex];
+    }
+   });
+});
 
 inputPassword.addEventListener('focus', (e) => {
     e.target.classList.add('active');
-})
+});
 
 inputPassword.addEventListener('blur', (e) => {
     e.target.classList.remove('active');
-})
+});
+
+
